66a2229f836e9663efb249bdc8e7e575dd16d1aa
zhangjialin <zhangjialin@itcast.cn>
2024-03-11 11:39:10 +0800

    第四次提交

--- src/main/java/mapper/userMapper.java
+++ src/main/java/mapper/userMapper.java
@@ -5,4 +5,6 @@ public interface userMapper {
     public void insert();
 
     public  void delete();
+
+
 }
---
+++ src/main/java/server/userServer.java
@@ -0,0 +1,5 @@
+package server;
+
+public interface userServer {
+    public void delete();
+}
